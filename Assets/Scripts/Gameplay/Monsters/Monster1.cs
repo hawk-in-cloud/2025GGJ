8977fef9b5d22890de7340af215bf595d8059675
E5note <87830062+E5note@users.noreply.github.com>
2025-06-28 09:02:59 +0800

    text

---
+++ Assets/Scripts/Gameplay/Monsters/Monster1.cs
@@ -0,0 +1,13 @@
+﻿using Gameplay.BaseItem;
+
+namespace Gameplay.Monsters
+{
+    public class Monster1 : BaseMonster
+    {
+        protected override void Init()
+        {
+            print("Monster1 Init");
+        }
+
+    }
+}
